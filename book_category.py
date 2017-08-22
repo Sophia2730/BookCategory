2924642500bff7282a6f5f157a4e799604755ae0
ChanSu <sophia@ChanSuui-MacBook-Pro.local>
2017-08-22 21:25:39 +0900

    카테고리 한개만 받아오도록 수정 & 임시 데이터

--- book_category.py
+++ book_category.py
@@ -1,23 +1,19 @@
 import requests
-import re
-from html2text import html2text
-import csv, numpy as np
+import csv
 from bs4 import BeautifulSoup
 
-hello = [0,0,0,0,0,0,0,0,0,0,0,0,0 ]
+data = [0 for i in range(20)]       # 크롤링한 데이터 값을 저장할 배열
 count = 0
 err = 0
-for i in range(12258356, 12258374):
+for i in range(12258356, 12258370):
     url = "http://book.naver.com/bookdb/book_detail.nhn?bid=" + str(i)
     html_result = requests.get(url)
     soup=BeautifulSoup(html_result.text,"html.parser")
     book_info = [0,0,0,0,0]
     try:
-        temp1 = soup.find(id='category_location1_depth')
-        temp1 = temp1.get_text()
-        temp2 = soup.find(id='category_location2_depth')
-        temp2 = temp2.get_text()
-        book_info[0] = temp1, temp2
+        temp = soup.find(id='category_location1_depth')
+        temp = temp.get_text()
+        book_info[0] = temp
 
         li_up=soup.find_all('div', attrs={'class':'thumb_type'})
         book_info[1] = li_up[0].find('img')['alt']
@@ -30,13 +26,15 @@ for i in range(12258356, 12258374):
 
         text_content = soup.find(id="pubReviewContent")
         book_info[4] = text_content.get_text()
-    except:
+    except:     # 에러 발생시 에러카운트 증가
         print(".", end= "")
         err += 1
-    else:
+    else:       # 에러 미발생시 data에 값을 저장
         print(".".rstrip())
-        hello[count] = [book_info[0],book_info[1],book_info[2],book_info[3],book_info[4]]
+        data[count] = [book_info[0],book_info[1],book_info[2],book_info[3],book_info[4]]
         count += 1
+
+
 list_out = []
 print("count : ", count, " err : ", err)
 for i in range(count):
@@ -44,7 +42,6 @@ for i in range(count):
         csvout = csv.DictWriter(fout, ['Category', 'Title', 'Author', 'Intro', 'Content'])
         csvout.writeheader()
         list_out.append(
-            {'Category': ''.join(hello[i][0]), 'Title': ''.join(hello[i][1]), 'Author': ''.join(hello[i][2]),
-             'Intro': ''.join(hello[i][3]), 'Content': ''.join(hello[i][4])})
+            {'Category': ''.join(data[i][0]), 'Title': ''.join(data[i][1]), 'Author': ''.join(data[i][2]),
+             'Intro': ''.join(data[i][3]), 'Content': ''.join(data[i][4])})
         csvout.writerows(list_out)
-
