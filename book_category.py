b5b6766276cb43c710c5a6a8d0dfe3f5d4c74fe2
ChanSu <sophia@ChanSuui-MacBook-Pro.local>
2017-08-29 01:35:00 +0900

    데이터 크롤링에서 작가정보 제외 및 임시데이터 커밋

--- book_category.py
+++ book_category.py
@@ -2,14 +2,15 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 
-data = [0 for i in range(20)]       # 크롤링한 데이터 값을 저장할 배열
+data = [0 for i in range(10000)]       # 크롤링한 데이터 값을 저장할 배열
 count = 0
 err = 0
-for i in range(12258356, 12258370):
+li_up = 0
+for i in range(12235899, 12257899):
     url = "http://book.naver.com/bookdb/book_detail.nhn?bid=" + str(i)
     html_result = requests.get(url)
     soup=BeautifulSoup(html_result.text,"html.parser")
-    book_info = [0,0,0,0,0]
+    book_info = [0,0,0,0]       # 카테고리, 책이름, 책소개, 출판사 서평
     try:
         temp = soup.find(id='category_location1_depth')
         temp = temp.get_text()
@@ -18,20 +19,17 @@ for i in range(12258356, 12258370):
         li_up=soup.find_all('div', attrs={'class':'thumb_type'})
         book_info[1] = li_up[0].find('img')['alt']
 
-        text_author = soup.find(id="authorIntroContent")
-        book_info[2] = text_author.get_text()
-
         text_intro = soup.find(id="bookIntroContent")
-        book_info[3] = text_intro.get_text()
+        book_info[2] = text_intro.get_text()
 
         text_content = soup.find(id="pubReviewContent")
-        book_info[4] = text_content.get_text()
+        book_info[3] = text_content.get_text()
     except:     # 에러 발생시 에러카운트 증가
         print(".", end= "")
         err += 1
     else:       # 에러 미발생시 data에 값을 저장
         print(".".rstrip())
-        data[count] = [book_info[0],book_info[1],book_info[2],book_info[3],book_info[4]]
+        data[count] = [book_info[0],book_info[1],book_info[2],book_info[3]]
         count += 1
 
 
@@ -39,9 +37,9 @@ list_out = []
 print("count : ", count, " err : ", err)
 for i in range(count):
     with open('./book_data.csv', "w+", encoding='utf-8') as fout:
-        csvout = csv.DictWriter(fout, ['Category', 'Title', 'Author', 'Intro', 'Content'])
+        csvout = csv.DictWriter(fout, ['Category', 'Title', 'Intro', 'Content'])
         csvout.writeheader()
         list_out.append(
-            {'Category': ''.join(data[i][0]), 'Title': ''.join(data[i][1]), 'Author': ''.join(data[i][2]),
-             'Intro': ''.join(data[i][3]), 'Content': ''.join(data[i][4])})
+            {'Category': ''.join(data[i][0]), 'Title': ''.join(data[i][1]),
+             'Intro': ''.join(data[i][2]), 'Content': ''.join(data[i][3])})
         csvout.writerows(list_out)
